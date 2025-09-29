391664ba6306d1332b7ab0f56b95de8a385621c8
Rupeshsoft <rssoftsoft@gmail.com>
2025-09-29 20:29:02 +0530

    Update main.java
    It will generate same hash code for multiple instances.
    
    output :
    =========
    
    705927765
    
    705927765

--- main.java
+++ main.java
@@ -1,1 +1,26 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
 
+class Singleton{
+    private static Singleton instance = null; 
+    private Singleton(){}
+    public static Singleton getInstance(){
+        if(instance == null ){
+            instance = new Singleton();
+        }
+        
+        return instance;
+    }
+}
+
+class Main {
+    public static void main(String[] args) throws NoSuchMethodException {
+        Singleton object = Singleton.getInstance();
+        Singleton object1 = Singleton.getInstance();
+        
+        System.out.println(object.hashCode());
+        System.out.println(object1.hashCode());
+        
+    }
+}
